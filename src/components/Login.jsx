f51948aedf3c4fa463882b92cb3189f6f25fb74a
Panchana Madara <panchanamadara16314@gmail.com>
2022-11-08 00:15:35 +0530

    Project initialized and the sign up and log in pages were created

---
+++ src/components/Login.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="grid grid-cols-2 w-[1200px] relative bg-white rounded-[15px] overflow-hidden drop-shadow-2xl">
+      <img
+        alt="logingImg"
+        src={`images/loginImg.png`}
+        className="w-[600px]  h-full aspect-square"
+      />
+      <img
+        alt="logo"
+        src={"images/logo_white.png"}
+        className="w-[200px] absolute top-[30px] left-[20px]"
+      />
+      <div className="p-[50px] flex flex-col justify-between">
+        <div>
+          <h1 className="font-semibold text-primaryGreen text-[15px]">
+            Welcome Back!
+          </h1>
+          <p className="font-bold text-secondarGreen text-[60px] leading-none">
+            {" "}
+            LogIn
+          </p>
+        </div>
+        <form>
+          <div class="form-group mb-6">
+            <label class="form-label font-semibold inline-block mb-1 text-gray-700">
+              Email address :
+            </label>
+            <input
+              type="email"
+              class="form-control w-[400px] block px-3 py-1.5 text-[14px] font-normal text-gray-700 bg-white bg-clip-padding  border-b-[1px] border-gray-300 transition ease-in-out focus:text-gray-700 focus:bg-white focus:border-secondarGreen focus:outline-none"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            />
+          </div>
+          <div class="form-group mb-6">
+            <label class="form-label font-semibold inline-block mb-1 text-gray-700">
+              Password :
+            </label>
+            <input
+              type="password"
+              class="form-control w-[400px] block px-3 py-1.5 text-[14px] font-normal text-gray-700 bg-white bg-clip-padding border-b-[1px] border-gray-300 transition ease-in-out focus:text-gray-700 focus:bg-white focus:border-secondarGreen focus:outline-none"
+              placeholder="Password"
+            />
+          </div>
+          <button
+            type="submit"
+            class="px-6 py-2.5 mt-2 bg-secondarGreen text-white font-medium text-base leading-tight uppercase rounded shadow-md hover:bg-primaryGreen hover:shadow-lg focus:bg-primaryGreen focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primaryGreen active:shadow-lg transition duration-150 ease-in-out">
+            Log In
+          </button>
+          <p class="text-gray-800 mt-2">
+            Not a member?{" "}
+            <span class="text-secondarGreen hover:text-primaryGreen focus:text-primaryGreen transition duration-200 ease-in-out">
+              Register
+            </span>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
