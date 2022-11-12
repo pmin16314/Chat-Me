8a21f630b31f06fe9a4918f008855b19ea654221
Panchana Madara <panchanamadara16314@gmail.com>
2022-11-12 12:29:21 +0530

    edit search

--- src/components/sidebar/Search.jsx
+++ src/components/sidebar/Search.jsx
@@ -1,9 +1,93 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import {
+  collection,
+  query,
+  where,
+  getDoc,
+  getDocs,
+  setDoc,
+  doc,
+  updateDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+import { db } from "../../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Search = () => {
+  const [findUser, setFindUser] = useState("");
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(false);
+
+  const { currentUser } = useContext(AuthContext);
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", findUser)
+    );
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+
+        console.log(doc.data());
+      });
+    } catch (err) {
+      setError(true);
+      console.log(err);
+    }
+  };
+
+  const handleClick = async () => {
+    //check whether the chat available between two or not. if not create it
+    const combinedUserIds =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+    try {
+      const res = await getDoc(doc(db, "chats", combinedUserIds));
+
+      if (!res.exists()) {
+        //create a chat in chats collection
+        console.log("ll");
+        await setDoc(doc(db, "chats", combinedUserIds), { messages: [] });
+
+        //create user chat
+
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedUserIds + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [combinedUserIds + ".dateAndTime"]: serverTimestamp(),
+        });
+        await updateDoc(doc(db, "userChats", user.id), {
+          [combinedUserIds + ".userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedUserIds + ".dateAndTime"]: serverTimestamp(),
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+
+    //
+  };
+
+  const handleKey = (key) => {
+    key.code === "Enter" && handleSearch();
+  };
+
+  setUser(null);
+  setFindUser("");
+
   return (
     <div className="p-2 mx-3 flex flex-col flex-none items-start space-y-2 border-b-[1px] border-secondarGreen">
-      <div className="flex flex-row items-stretch">
+      <div id="search bar" className="flex flex-row items-stretch">
         <span className="flex items-center px-3 py-1.5 text-base text-white font-normal text-center whitespace-nowrap">
           <svg
             aria-hidden="true"
@@ -22,17 +106,30 @@ const Search = () => {
           type="search"
           className="relative flex-auto min-w-0 block px-3 py-1.5 text-[18px] font-light text-white bg-transparent transition ease-in-out outline-none placeholder:italic placeholder:text-gray-50 placeholder:font-extralight  m-0"
           placeholder="Find a user..."
-          aria-label="Search"
+          onChange={(e) => {
+            setFindUser(e.target.value);
+          }}
+          onKeyDown={handleKey}
+          value={findUser}
         />
+        {error && (
+          <span className="ml-2 text-red-700">Something went wrong</span>
+        )}
       </div>
-      <div className="p-[10px] w-full rounded-[10px] flex flex-row items-center hover:bg-secondarGreen">
-        <img
-          src="https://images.pexels.com/photos/1391498/pexels-photo-1391498.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          className="w-[50px] h-[50px] rounded-[50%] object-cover"
-          alt="profile"
-        />
-        <span className="ml-[15px] font-medium text-gray-50 ">Jane</span>
-      </div>
+      {user && (
+        <div
+          onClick={handleClick}
+          className="p-[10px] w-full rounded-[10px] flex flex-row items-center hover:bg-secondarGreen">
+          <img
+            src={user.photoURL}
+            className="w-[50px] h-[50px] rounded-[50%] object-cover"
+            alt="profile"
+          />
+          <span className="ml-[15px] font-medium text-gray-50 ">
+            {user.displayName}
+          </span>
+        </div>
+      )}
     </div>
   );
 };
